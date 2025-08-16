8773e221564c3b4c3c7396b2ae89d30d25d61350
tkn1223 <taka12230000@gmail.com>
2025-08-16 17:54:51 +0900

    コメントの投稿機能を実装

--- my-app/resources/js/Pages/Review/Create.jsx
+++ my-app/resources/js/Pages/Review/Create.jsx
@@ -8,24 +8,53 @@ import {
     Text,
     Textarea,
 } from "@chakra-ui/react";
+import { router } from "@inertiajs/react";
+import { useState } from "react";
 
 const Create = (props) => {
+    const [values, setValues] = useState({
+        shop_id: props.shop.id,
+        rating: 1,
+        comment: "",
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        router.post(route("review.store"), values);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value,
+        });
+    };
+
     return (
         <Box>
             <Heading>レビューを投稿</Heading>
             <Text>{props.shop.name}</Text>
-            <form>
-                <FormControl>
+            <form onSubmit={handleSubmit}>
+                <FormControl isRequired>
                     <FormLabel htmlFor="rating" fontWeight={"bold"}>
                         評価
                     </FormLabel>
-                    <input type="number" name="rating" />
+                    <input
+                        type="number"
+                        name="rating"
+                        onChange={handleChange}
+                    />
                 </FormControl>
-                <FormControl>
+                <FormControl isRequired>
                     <FormLabel htmlFor="comment" fontWeight={"bold"}>
                         コメント
                     </FormLabel>
-                    <Textarea type="text" name="comment"></Textarea>
+                    <Textarea
+                        type="text"
+                        name="comment"
+                        onChange={handleChange}
+                    ></Textarea>
                 </FormControl>
                 <Button type="submit" colorScheme="blue" mt={4}>
                     投稿する
