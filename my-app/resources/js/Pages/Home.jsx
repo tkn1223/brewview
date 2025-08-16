115930a5db3a2afdad36d971c901334b907570fe
tkn1223 <taka12230000@gmail.com>
2025-08-16 14:13:44 +0900

    homeから詳細ページへの遷移

--- my-app/resources/js/Pages/Home.jsx
+++ my-app/resources/js/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import MainLayout from "@/Layouts/MainLayout";
 import { StarIcon } from "@chakra-ui/icons";
 import { Box, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Link } from "@inertiajs/react";
 
 const Home = (props) => {
     return (
@@ -15,29 +16,37 @@ const Home = (props) => {
                 </Heading>
                 <VStack spacing={4} align="{stretch}">
                     {props.shops.map((shop) => (
-                        <Box
-                            key={shop.id}
-                            p={4}
-                            borderWidth="1px"
-                            borderRadius={"lg"}
-                            overflow={"hidden"}
-                            boxShadow={"lg"}
-                        >
-                            <HStack spacing={4}>
-                                <Image
-                                    boxSize="100px"
-                                    objectFit="cover"
-                                    src="https://www.biz.ne.jp/sys/image.php?data=NpMPnIx5xd%2FCjA8rawKl1eDJL8Xa24KNkuGaLJ35tUvnOwKVposTmZ%2BMDFGLb8%2Bq"
-                                    alt={shop.name}
-                                />
-                                <VStack align={"start"}>
-                                    <Heading as="h3" size="md">
-                                        {shop.name}
-                                    </Heading>
-                                    <Text>{shop.description}</Text>
-                                </VStack>
-                            </HStack>
-                        </Box>
+                        <Link href={`/shop/${shop.id}`} key={shop.id}>
+                            <Box
+                                key={shop.id}
+                                p={4}
+                                borderWidth="1px"
+                                borderRadius={"lg"}
+                                overflow={"hidden"}
+                                boxShadow={"lg"}
+                                _hover={{
+                                    transform: "translateY(-2px)",
+                                    boxShadow: "xl",
+                                    borderColor: "blue.500",
+                                }}
+                                transition="all 0.2s"
+                            >
+                                <HStack spacing={4}>
+                                    <Image
+                                        boxSize="100px"
+                                        objectFit="cover"
+                                        src="https://www.biz.ne.jp/sys/image.php?data=NpMPnIx5xd%2FCjA8rawKl1eDJL8Xa24KNkuGaLJ35tUvnOwKVposTmZ%2BMDFGLb8%2Bq"
+                                        alt={shop.name}
+                                    />
+                                    <VStack align={"start"}>
+                                        <Heading as="h3" size="md">
+                                            {shop.name}
+                                        </Heading>
+                                        <Text>{shop.description}</Text>
+                                    </VStack>
+                                </HStack>
+                            </Box>
+                        </Link>
                     ))}
                 </VStack>
                 <Heading
