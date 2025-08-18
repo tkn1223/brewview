3bdf334e62fada8dc2338fddd422ac2845743dc6
tkn1223 <taka12230000@gmail.com>
2025-08-18 23:57:23 +0900

    shopsテーブルへのカラム追加とshop_imagesテーブルの作成

---
+++ my-app/database/migrations/2025_08_18_234538_create_shop_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shop_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shop_id')->constrained();
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('file_type');
+            $table->string('file_size');
+            $table->string('file_extension');
+            $table->string('file_mime');
+            $table->string('file_original_name');
+            $table->string('file_original_path');
+            $table->string('file_original_type');
+            $table->unsignedBigInteger('thumbnail_id')->nullable();
+            $table->foreign('thumbnail_id')->references('id')->on('shop_images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shop_images');
+    }
+};
